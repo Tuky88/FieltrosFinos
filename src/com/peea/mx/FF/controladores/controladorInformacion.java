e27b5dd80fcf31191e1940793033265a6e230625
user <user@user-PC>
2019-01-31 12:59:04 -0600

    PRUEBAS CODIGO QR

--- src/com/peea/mx/FF/controladores/controladorInformacion.java
+++ src/com/peea/mx/FF/controladores/controladorInformacion.java
@@ -7,8 +7,12 @@ package com.peea.mx.FF.controladores;
 
 import com.barcodelib.barcode.QRCode;
 import com.peea.mx.FF.iFrame.informacioniFrame;
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -29,13 +33,26 @@ public class controladorInformacion {
 
         @Override
         public void actionPerformed(ActionEvent ae) {
-            QRCode qr= new QRCode();
-            qr.setData("HOLA PERRO");
-            qr.setDataMode(QRCode.MODE_BYTE);
-            qr.setUOM(50);
-            qr.setLeftMargin(0);
-            
+            try {
+                QRCode qr= new QRCode();
+                qr.setData("Nombre:HOLA PERRO"
+                        + "Edad: 19");
+                qr.setDataMode(QRCode.MODE_BYTE);
+                qr.setUOM(0);
+                qr.setResolution(500);
+                qr.setRightMargin(0.000f);
+                qr.setTopMargin(0.000f);
+                qr.setLeftMargin(0.000f);
+                qr.setBottomMargin(0.000f);
+                qr.setModuleSize(10);
+                String f="C:\\FieltrosFinos\\src\\com\\peea\\mx\\FF\\imagenes\\codigo.png";
+                qr.renderBarcode(f);
+                Desktop d= Desktop.getDesktop();
+                d.open(new File(f));
 //To change body of generated methods, choose Tools | Templates.
+            } catch (Exception ex) {
+                Logger.getLogger(controladorInformacion.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
     
